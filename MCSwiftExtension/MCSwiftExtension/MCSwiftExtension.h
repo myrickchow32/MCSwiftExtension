465eabab14ec2f40d6655b3aae172d1ab3012a0a
Myrick Chow <myrick@redso.com.hk>
2021-03-22 14:41:33 +0800

    Init project

---
+++ MCSwiftExtension/MCSwiftExtension/MCSwiftExtension.h
@@ -0,0 +1,18 @@
+//
+//  MCSwiftExtension.h
+//  MCSwiftExtension
+//
+//  Created by Myrick Chow on 22/3/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MCSwiftExtension.
+FOUNDATION_EXPORT double MCSwiftExtensionVersionNumber;
+
+//! Project version string for MCSwiftExtension.
+FOUNDATION_EXPORT const unsigned char MCSwiftExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MCSwiftExtension/PublicHeader.h>
+
+
---
+++ MCSwiftExtension/MCSwiftExtensionExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MCSwiftExtensionExample
+//
+//  Created by Myrick Chow on 22/3/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
