a5e37807b654b21b086d0698826dd8f65499f7b2
Myrick Chow <myrick@redso.com.hk>
2021-03-24 17:13:53 +0800

    Add Date extension

---
+++ MCSwiftExtension/DateExtension.swift
@@ -0,0 +1,9 @@
+import CoreFoundation
+extension Date {
+  func printing() {
+    print("This is a printing function in Date")
+  }
+  public func publicPrint() {
+    print("This is a publicPrint function in Date")
+  }
+}
